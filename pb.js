4385a2155e9aac6a3954fa41ec38087b63c253b9
tigerlily-he <yonglinlilyhe@gmail.com>
2016-04-22 22:24:26 -0400

    Added everything from the AppNexus Final version

---
+++ pb.js
@@ -0,0 +1,751 @@
+//Basic(less than 6 letters)
+//pos= part of speech
+var evade ={
+    'pos': 'verb',
+    'def': 'to dodge',
+    'translation': {
+     'spanish': 'evadir',
+        'english':'evade'
+    }
+};
+
+var feign ={
+    'pos': 'verb',
+    'def': 'pretend',
+    'translation': {
+     'spanish': 'fingir',
+        'english':'feign'
+    }
+};
+
+var grim ={
+    'pos': 'adjective',
+    'def': 'serious',
+    'translation':{
+     'spanish':'serio(a)',
+        'english':'grim'
+    }
+};
+
+var exalt ={
+    'pos': 'verb',
+    'def': 'to praise',
+    'translation':{
+     'spanish': 'exaltar',
+        'english':'exalt'
+    }
+};
+
+var fetid ={
+    'pos': 'adjective',
+    'def': 'malodorous',
+    'translation':{
+     'spanish': 'fetido(a)',
+        'english':'fetid'
+    }
+};
+
+var filch ={
+    'pos': 'verb',
+    'def': 'to steal',
+    'translation':{
+     'spanish': 'robar',
+        'english':'filch'
+    }
+};
+
+var flair ={
+    'pos': 'noun',
+    'def': 'talent',
+    'translation':{
+     'spanish': 'talento',
+        'english':'flair'
+    }
+};
+
+var flout ={
+    'pos': 'verb',
+    'def': 'comtempt',
+    'translation':{
+     'spanish': 'desobeder',
+        'english':'flout'
+    }
+};
+
+var foil ={
+    'pos': 'verb',
+    'def': 'defeat',
+    'translation':{
+     'spanish': 'desbaratar',
+        'english': 'foil'
+    }
+};
+
+var forgo ={
+    'pos': 'verb',
+    'def': 'give up',
+    'translation':{
+     'spanish': 'abstenerse',
+        'english':'forgo'
+    }
+};
+
+var glut ={
+    'pos': 'noun',
+    'def': 'fill to excess',
+    'translation':{
+     'spanish': 'exceso(m)',
+        'english':'glut'
+    }
+};
+
+var guile ={
+    'pos': 'noun',
+    'def': 'deceit',
+    'translation':{
+     'spanish': 'astucia',
+        'english': 'guile'
+    }
+};
+
+var guru ={
+    'pos': 'noun',
+    'def': 'expert',
+    'translation':{
+     'spanish': 'gur\372',
+     'english': 'guru',
+    }
+};
+
+var hoard ={
+    'def': '(v.) - save',
+    'translation':{
+     'spanish': 'acumular',
+     'english': 'hoard'
+    }
+};
+
+var impel ={
+    'pos': 'verb',
+    'def': 'force onward',
+    'translation':{    
+     'spanish': 'impulsar',
+     'english': 'impel'
+    }
+};
+
+var inane ={
+    'pos': 'adjective',
+    'def': 'silly',
+    'translation':{
+     'spanish': 'sonso',
+     'english': 'inane'
+    }
+};
+
+var inept ={
+    'pos': 'adjective',
+    'def': 'lacking skill',
+    'translation':{
+     'spanish': 'inepto',
+     'english': 'inept'
+    }
+};
+
+var inert ={
+    'pos': 'adjective',
+    'def': 'inactive',
+    'translation':{
+     'spanish': 'inm\363vil',
+     'english': 'inert'
+    }
+};
+
+var iota ={
+    'pos': 'noun',
+    'def': 'small quality',
+    'translation':{
+     'spanish': 'pizca',
+     'english': 'iota'
+    }
+};
+
+var jest ={
+    'pos':'noun',
+    'def': 'joke',
+    'translation':{
+     'spanish': 'broma',
+     'english': 'jest'
+    }
+};
+
+var jibe ={
+    'pos': 'verb',
+    'def': 'mock',
+    'translation':{
+     'spanish': 'burlarse',
+     'english': 'jibe'
+    }
+};
+
+var lax ={
+    'pos': 'adjective',
+    'def': 'careless',
+    'translation':{
+     'spanish': 'relajado(a)',
+     'english': 'lax'
+    }
+};
+
+var leery ={
+    'pos': 'adjective',
+    'def': 'cautious',
+    'translation':{
+     'spanish': 'cauteloso',
+     'english': 'leery'
+    }
+};
+
+var limp ={
+    'pos': 'adjective',
+    'def': 'no energy',
+    'translation':{
+     'spanish': 'flojo(a)',
+     'english': 'limp'
+    }
+};
+
+var lull ={
+    'pos': 'verb',
+    'def': 'soothe',
+    'translation':{
+     'spanish': 'calmar',
+     'english': 'lull'
+    }
+};
+
+var lush ={
+    'pos': 'adjective',
+    'def': 'abundant',
+    'translation':{
+     'spanish': 'lujoso(a)',
+     'english': 'lush'
+    }
+};
+
+var meek ={
+    'pos': 'adjective',
+    'def': 'obedient',
+    'translation':{
+     'spanish': 'd\363cil',
+     'english': 'meek'    
+    }
+};
+
+//Medium(6-9 letters)
+var esteem = {
+    'pos': 'adjective',
+    'def': 'respect',
+    'translation':{
+     'spanish': 'apreciar',
+     'english': 'esteem'
+    }
+};
+
+var exacting = {
+    'pos': 'adjective',
+    'def': 'extremely demanding',
+    'translation':{
+     'spanish': 'exigente',
+     'english': 'exacting'
+    }
+};
+
+var exegesis = {
+    'pos': 'noun',
+    'def': 'explanation',
+    'translation':{
+     'spanish': 'explicaci\363n',
+     'english': 'exegesis'
+    }
+};
+
+var faction = {
+    'pos': 'noun',
+    'def': '(n.)- party',
+    'translation':{
+     'spanish': 'faccion',
+     'english': 'faction'
+    }
+};
+
+var fathom = {
+    'pos': 'verb',    
+    'def': 'comprehend',
+    'translation':{
+     'spanish': 'comprender',
+     'english': 'fathom',
+    }
+};
+
+var fervent = {
+    'pos': 'adjective',
+    'def': 'ardent',
+    'translation':{
+     'spanish': 'enf\341tico',
+     'english': 'fervent'
+
+    }
+};
+
+var forswear = {
+    'pos': 'adjective',
+    'def': 'abandon',
+    'translation':{
+     'spanish': 'abandonar',
+     'english': 'forswear'
+    }
+};
+
+var fraught = {
+    'pos': 'adjective',
+    'def': 'filled',
+    'translation':{
+     'spanish': 'lleno',
+     'english': 'fraught'
+    }
+};
+
+var fulminate = {
+    'pos': 'verb',
+    'def': 'criticized strongly',
+    'translation':{
+     'spanish': 'fulminante',
+     'english': 'fulminate'
+    }
+};
+
+var gratify = {
+    'pos': 'verb',
+    'def': 'please',
+    'translation':{
+     'spanish': 'gratificar',
+     'english': 'gratify'
+    }
+};
+
+var grievious = {
+    'pos': 'adjective',
+    'def': 'great pain',
+    'translation':{
+     'spanish':  'doloroso',
+     'english': 'grievious'
+    }
+};
+
+var husband = {
+    'pos': 'verb',
+    'def': ' save',
+    'translation':{
+     'spanish': 'administrar/ ahorrar',
+     'english': 'husband'
+    }
+
+};
+
+var immense = {
+    'pos': 'adjective',
+    'def': 'enormous',
+    'translation':{
+     'spanish': 'inmenso',
+     'english': 'immense'
+    }
+};
+
+var impassive = {
+    'pos': 'adjective',
+    'def': 'fair',
+    'translation':{
+     'spanish': 'imperturbable',
+     'english': 'impassive'
+    }
+};
+
+var impasse = {
+    'pos': 'noun',
+    'def': 'predicament',
+    'translation':{
+     'spanish': 'predicamento',
+     'english': 'impasse'
+    }
+};
+
+var ingrained = {
+    'pos': 'adjective',
+    'def': 'firmly rooted',
+    'translation':{
+     'spanish': 'arraigado(a)',
+     'english': 'ingrained'
+    }
+};
+
+var innocuous = {
+    'pos': 'adjective',
+    'def': 'harmless',
+    'translation':{
+     'spanish': 'inofensivo',
+     'english': 'innocuous'
+    }
+};
+
+var inundate = {
+    'pos': 'verb',
+    'def': 'flood',
+    'translation':{
+     'spanish': 'inundar',
+     'english': 'inundate'
+    }
+};
+
+var irascible= {
+    'pos': 'adjective',
+    'def': 'irritable',
+    'translation':{
+     'spanish': 'irritable',
+     'english': 'irascible'
+    }
+};
+
+var jaundiced= {
+    'pos': 'adjective',
+    'def': 'prejudiced',
+    'translation':{
+     'spanish': 'resentido(a)',
+     'english': 'jaundiced'
+    }
+};
+
+var layman= {
+    'pos': 'noun',
+    'def': 'not professional',
+    'translation':{
+     'spanish': 'profano(a)',
+     'english': 'layman'
+    }
+};
+
+var litigious= {
+    'pos': 'adjective',
+    'def': 'ready to take disputes',
+    'translation':{
+     'spanish': 'litigante',
+     'english': 'litigious'
+    }
+};
+
+var magnify= {
+    'pos': 'verb',
+    'def': 'to make something bigger',
+    'translation':{
+     'spanish': 'aumentar',
+     'english': 'magnify'
+    }
+};
+
+
+var manifest= {
+    'pos': 'adjective',
+    'def': 'evident',
+    'translation':{
+     'spanish': 'manifestar',
+     'english': 'manifest'
+    }
+};
+
+
+var marshal= {
+    'pos': 'verb',
+    'def': 'to organize',
+    'translation':{
+     'spanish': 'dirigir',
+     'english': 'marshal'
+    }
+};
+
+
+var menace= {
+    'pos': 'verb',
+    'def': 'threat',
+    'translation':{
+     'spanish': 'amenaza',
+     'english': 'menace'
+    }
+};
+
+var pristine= {
+    'pos': 'adjective',
+    'def': 'untouched',
+    'translation':{
+     'spanish': 'inmaculado(a)',
+     'english': 'pristine'
+    }
+};
+
+
+
+//Hard Level
+
+var evanescent= {
+    'pos': 'adjective',
+    'def': 'fleeting',
+    'translation':{
+     'spanish': 'fugaz',
+     'english': 'evanescent'
+    }
+};
+
+var exceptional= {
+    'pos': 'adjective',
+    'def': 'good',
+    'translation':{
+     'spanish': 'extraordinario',
+     'english': 'exceptional'
+    }
+};
+
+var excruciating= {
+    'pos': 'adjective',
+    'def': 'very intense',
+    'translation':{
+     'spanish': 'insorportable',
+     'english': 'excruciating'
+    }
+};
+
+var fastidious= {
+    'pos': 'adjective',
+    'def': 'hard to please',
+    'translation':{
+     'spanish': 'meticuloso',
+     'english': 'fastidious'
+    }
+};
+
+var felicitous= {
+    'pos': 'adjective',
+    'def': 'well chosen',
+    'translation':{
+     'spanish': 'acertado(a)',
+     'english': 'felicitous'
+    }
+};
+
+var formidable= {
+    'pos': 'adjective',
+    'def': 'menacing',
+    'translation':{
+     'spanish': 'amenazante',
+     'english': 'formidable'
+    }
+};
+
+var fraudulent= {
+    'pos': 'adjective',
+    'def': 'cheating',
+    'translation':{
+     'spanish': 'fraudulento(a)',
+     'english': 'fraudulent'
+    }
+};
+
+//Choose Words
+
+
+var getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var getNRandomWords = function(wordArray, num) {
+	var thisLevelWords = {
+		 index : [],
+		result : []
+	}
+   
+
+	
+    for(var n=0; n<num; n++){
+             var proceed = false;
+             while(proceed !== true){
+                 var temp = getRandomInt(0, wordArray.length-1);
+                 if(thisLevelWords.index.indexOf(temp) === -1){
+                    thisLevelWords.index.push(wordArray[temp]);
+                     thisLevelWords.result.push(
+					 
+					{
+						 indexIndex: thisLevelWords.index.length-1, 
+						 vocab: wordArray[temp].translation.english
+						});
+                     thisLevelWords.result.push(
+					 {
+						 indexIndex: thisLevelWords.index.length-1,
+						 vocab: wordArray[temp].translation.spanish
+						});
+                     proceed = true;
+                 }
+             }           
+     }
+
+     num = num * 2;
+     while(num !== 0) {
+        //pick a random int between 0 - 12
+        //this is equal to the remaining words inside the array
+        //after every word, we decrease the integer, so it is 0 - 11, 0 - 10, etc.
+        var randomandajar=getRandomInt(0, num);
+        num -= 1; 
+        
+        var temp = thisLevelWords.result[randomandajar];
+        thisLevelWords.result[randomandajar] = thisLevelWords.result[num];
+        thisLevelWords.result[num] = temp;
+     }
+     return thisLevelWords;
+};
+
+var easyWords= [guile, evade, feign, grim, exalt, fetid, filch, flair, flout, foil, forgo, glut, grim, guile, guru, hoard, impel, inane, inept, inert, iota, jest, jibe, lax, leery, limp, lull, lush, meek, ];
+
+var mediumWords= [esteem, exacting, faction, fathom, fervent, forswear, fraught,fulminate, gratify, grievious, husband, immense, impassive, impasse, ingrained, innocuous, inundate, irascible, jaundiced, layman, litigious, magnify, manifest, marshal, menace, pristine];
+
+var hardWords= [evanescent, exceptional, excruciating, fastidious, felicitous, formidable, fraudulent];
+
+
+/** function for choosing N words
+    params: level, num,
+**/
+var chooseWords= function(level, num) {
+    var lays = [];
+    //easy levels
+     if(level>=1 && level<= 5){
+        lays = getNRandomWords(easyWords, num);
+
+     }
+     else if(level>5 && level<11){
+        lays = getNRandomWords(mediumWords, num);
+     }
+     else if (level>=11 && level<=15){
+        lays = getNRandomWords(hardWords, num);
+     }
+     return lays;
+};
+
+$(document).ready(function(){
+    var score = 0;
+    $('#score').text('Score: ' + score);
+    var countClick = 0;
+    var firstClick = null;
+    var secondClick = null;
+    var click_id_1 = null;
+    var click_id_2 = null;
+    var click_1;
+    var click_2;
+    var trueCheck = 0;
+    //var words = ["Alex" , "Alex" , "Ana" , "Ana" , "Mohammed" , "Mohammed" , "Boby" , "Boby" , "Dipali" , "Dipali" , "Wilson" , "Wilson"];
+    var flip = [];
+    for(var i=0; i < flip.length; i++){ 
+        flip[i] = false;
+    }
+        var words = chooseWords (1, 6);
+
+        console.log(words);
+
+    $('.box').click(function(){
+        countClick += 1;
+        
+        if(countClick ==1){
+            click_id_1 = $(this).attr('id');
+            click_1 = this;
+            firstClick = words.result[click_id_1];
+            $(this).text("" + firstClick.vocab);
+            $(this).children('img').hide();
+        }
+            
+        else if(countClick == 2){
+			  click_id_2 = $(this).attr('id');
+			if(click_id_1!=click_id_2){
+				
+            click_2 = this;
+            secondClick = words.result[click_id_2];
+            $(this).text("" + secondClick.vocab);
+            $(this).children('img').hide();
+          
+            if(firstClick.indexIndex == secondClick.indexIndex){
+                score = score + 100;
+                $('#score').text('Score: ' + score);
+                flip[click_id_1] = true;
+                flip[click_id_2] = true;
+			}
+           else{
+                setTimeout(function(){
+                $('#' + click_id_1).text('img');
+                $('#' + click_id_2).text('img');
+                click_1.innerHTML = "<img src='balloon.png'/>";
+                click_2.innerHTML = "<img src='balloon.png'/>";
+            }, 500);
+                score = score - 25;
+                $('#score').text('Score: ' + score);
+                }
+			countClick = 0;
+        
+                }
+          else{
+			  countClick-=1;
+		  }
+		}
+          
+
+                
+        for(var i=0; i<12; i++){
+            if(flip[i] === true){
+                trueCheck += 1;
+                }
+            }
+        
+        if(trueCheck >= 12){
+            $('#toggle').animate({opacity:'0.4'},"slow");
+            $('#p').html("<div id='msg' >You've Won!</div>");
+            $('#msg').animate({fontSize:'8em'},"slow");
+            $('#toggle').animate({opacity:'initial'},"fast");
+            $('#msg').remove();
+            trueCheck = 0;
+
+            }
+        else
+            {trueCheck = 0;}
+    });
+});
+ /*
+  $('#reset').click(function(){
+ 
+	
+        $('.box').text("");
+        var cards = document.getElementsByClassName("box");
+        for (var i = 0; i < cards.length; i++;) {
+            cards[i].innerHTML = "<img src='balloon.png'/>" 
+        };
+		
+        $('#score').text("Score: 0");
+        $('#toggle').animate({opacity:'initial'},"fast");
+        $('#msg').remove();
+        trueCheck = 0;
+    });
+	*/
+   
+	
+    //This equals a jquery object
+    //This jquery object references all the boxes that have a class that contains "box"
+    //the .box is how we defined that
+    //$('.box');
+
+
+    //sets a click handler to the box
+    //passes in a function
+    //this funciton is triggered whenever a click occur
+    
